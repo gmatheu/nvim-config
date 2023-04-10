32be10aeda00fd1f67b22dfcdc2cd23fe0bf6e1c
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-04-10 13:51:20 -0300

    Adding marks and tmux-awesome-manager plugins

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -93,35 +93,65 @@ return {
       }
     end,
   },
-  -- { "NvChad/extensions", branch = "v2.0" },}
-  --
-  -- {
-  --   "NvChad/base46",
-  --   branch = "v2.0",
-  --   build = function()
-  --     require("base46").load_all_highlights()
-  --   end,
-  -- },
-  --
-  -- {
-  --   "NvChad/ui",
-  --   branch = "v2.0",
-  --   lazy = false,
-  --   config = function()
-  --     require "nvchad_ui"
-  --   end,
-  -- },
 
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   init = require("core.utils").lazy_load "nvim-colorizer.lua",
-  --   config = function(_, opts)
-  --     require("colorizer").setup(opts)
-  --
-  --     -- execute colorizer as soon as possible
-  --     vim.defer_fn(function()
-  --       require("colorizer").attach_to_buffer(0)
-  --     end, 0)
-  --   end,
-  -- },
+  {
+    "otavioschwanck/tmux-awesome-manager.nvim",
+    config = function()
+      require("tmux-awesome-manager").setup {
+        per_project_commands = {
+          -- Configure your per project servers with
+          -- project name = { { cmd, name } }
+          api = { { cmd = "rails server", name = "Rails Server" } },
+          front = { { cmd = "yarn dev", name = "react server" } },
+        },
+        -- session_name = 'Neovim Terminals',
+        -- use_icon = false, -- use prefix icon
+        -- icon = ' ', -- Prefix icon to use
+        -- project_open_as = 'window', -- Open per_project_commands as.  Default: separated_session
+        -- default_size = '30%', -- on panes, the default size
+        -- open_new_as = 'window' -- open new command as.  options: pane, window, separated_session.
+      }
+    end,
+  },
+
+  {
+    "chentoast/marks.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("marks").setup {
+        -- whether to map keybinds or not. default true
+        default_mappings = true,
+        -- which builtin marks to show. default {}
+        builtin_marks = { ".", "<", ">", "^" },
+        -- whether movements cycle back to the beginning/end of buffer. default true
+        cyclic = true,
+        -- whether the shada file is updated after modifying uppercase marks. default false
+        force_write_shada = false,
+        -- how often (in ms) to redraw signs/recompute mark positions.
+        -- higher values will have better performance but may cause visual lag,
+        -- while lower values may cause performance penalties. default 150.
+        refresh_interval = 250,
+        -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
+        -- marks, and bookmarks.
+        -- can be either a table with all/none of the keys, or a single number, in which case
+        -- the priority applies to all marks.
+        -- default 10.
+        sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+        -- disables mark tracking for specific filetypes. default {}
+        excluded_filetypes = {},
+        -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+        -- sign/virttext. Bookmarks can be used to group together positions and quickly move
+        -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+        -- default virt_text is "".
+        bookmark_0 = {
+          sign = "⚑",
+          virt_text = "hello world",
+          -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
+          -- defaults to false.
+          annotate = false,
+        },
+        mappings = {},
+      }
+    end,
+  },
 }
