d10c3cb82aaf4ff05517874c83a28cbf7e94b302
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-08-10 14:32:42 -0300

    zk: Addin plugin

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -427,8 +427,33 @@ return {
   --
 
   {
+    "mickael-menu/zk-nvim",
+    cmd = { "ZkIndex", "ZkNew" },
+    config = function()
+      require("zk").setup {
+        -- See Setup section below
+        picker = "telescope",
+
+        lsp = {
+          -- `config` is passed to `vim.lsp.start_client(config)`
+          config = {
+            cmd = { "zk", "lsp" },
+            name = "zk",
+            -- on_attach = ...
+            -- etc, see `:h vim.lsp.start_client()`
+          },
+
+          -- automatically attach buffers in a zk notebook that match the given filetypes
+          auto_attach = {
+            enabled = true,
+            filetypes = { "markdown" },
+          },
+        },
+      }
+    end,
+  },
+  {
     "fedepujol/move.nvim",
     cmd = { "MoveLine", "MoveBlock"}
   },
-
 }
