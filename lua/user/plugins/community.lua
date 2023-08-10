340546a1f38cde045eb4fbc33622afbb36c687e8
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2023-08-10 14:15:31 -0300

    community: rainbow-delimiters, git-blame, hardtime, bad-practices

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -1,10 +1,17 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.editing-support/rainbow-delimiters-nvim" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
  -- { import = "astrocommunity.colorscheme.catppuccin" }
  { import = "astrocommunity.debugging.nvim-bqf" },
- { import = "astrocommunity.git.git-blame-nvim" }
+ -- { import = "astrocommunity.diagnostics.lsp_lines-nvim"},
+ { import = "astrocommunity.git.git-blame-nvim" },
+
+ { import = "astrocommunity.workflow.hardtime-nvim" },
+ { import = "astrocommunity.workflow.bad-practices-nvim" },
+
+ -- { import = "astrocommunity.pack.python" },
 }
