c01db2d19a00a2457fcca18b6116ba76defee522
Gonzalo Matheu (framework) <gmatheu@simtlix.com>
2024-03-04 08:49:21 -0300

    cmp: Adding autocomplete cmp-cmdline

--- lua/user/plugins/core.lua
+++ lua/user/plugins/core.lua
@@ -82,6 +82,29 @@ return {
           vim.fn.stdpath "config" .. "/lua/user/snippets",
         },
       }
+      local cmp = require "cmp"
+      -- `/` cmdline setup.
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
     end,
   },
   -- You can disable default plugins as follows:
