8e907de12d2dcbd8b69d8151ce81ae013b7a4f2d
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-04-04 10:42:35 -0300

    to-v4: Moving community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,27 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.editing-support/rainbow-delimiters-nvim" },
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- { import = "astrocommunity.colorscheme.catppuccin" }
+
+  { import = "astrocommunity.debugging.nvim-bqf" },
+  { "junegunn/fzf", event = "VeryLazy" },
+  { "junegunn/fzf.vim", event = "VeryLazy" },
+
+  -- { import = "astrocommunity.diagnostics.lsp_lines-nvim"},
+  { import = "astrocommunity.git.git-blame-nvim" },
+
+  { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.workflow.bad-practices-nvim" },
+
+  { import = "astrocommunity.project.project-nvim" },
+
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  -- { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.java" },
+
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity/file-explorer/oil-nvim" },
 }
