2d8b3cbabfc4cf7df77d6931e607984157d27af9
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-04-04 14:10:12 -0300

    to-v4: Migrating treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
@@ -10,7 +6,33 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
-      -- add more arguments for adding more treesitter parsers
+      "python",
+      "javascript",
+      "typescript",
+      "java",
+      "sql",
+      "tsx",
+      "json",
     })
+
+    opts.auto_install = true
+    opts.highlight = {
+      -- `false` will disable the whole extension
+      enable = true,
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    }
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        scope_incremental = "<CR>",
+        node_incremental = "<TAB>",
+        node_decremental = "<S-TAB>",
+      },
+    }
   end,
 }
