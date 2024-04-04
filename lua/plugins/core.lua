d9a3b5bed751dfd7bdecee05fbfdd3d436b47f49
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-04-04 10:37:39 -0300

    to-v4: Moving core and trial plugins

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -27,32 +27,34 @@ return {
   { "stevearc/resession.nvim", enabled = false },
   { "Shatur/neovim-session-manager", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  {
-    "rebelot/heirline.nvim",
-    opts = function(_, opts)
-      local status = require "astronvim.utils.status"
-      opts.statusline = {
-        -- statusline
-        hl = { fg = "fg", bg = "bg" },
-        -- status.hl,
-        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-        status.component.git_branch(),
-        status.component.file_info { filetype = false, filename = {}, file_modified = {} },
-        status.component.git_diff(),
-        status.component.diagnostics(),
-        status.component.fill(),
-        status.component.cmd_info(),
-        status.component.fill(),
-        status.component.lsp(),
-        status.component.treesitter(),
-        status.component.nav(),
-        -- remove the 2nd mode indicator on the right
-      }
-
-      -- return the final configuration table
-      return opts
-    end,
-  },
+  --
+  -- TODO: Check migration to v4
+  -- {
+  --   "rebelot/heirline.nvim",
+  --   opts = function(_, opts)
+  --     local status = require "astronvim.utils.status"
+  --     opts.statusline = {
+  --       -- statusline
+  --       hl = { fg = "fg", bg = "bg" },
+  --       -- status.hl,
+  --       status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+  --       status.component.git_branch(),
+  --       status.component.file_info { filetype = false, filename = {}, file_modified = {} },
+  --       status.component.git_diff(),
+  --       status.component.diagnostics(),
+  --       status.component.fill(),
+  --       status.component.cmd_info(),
+  --       status.component.fill(),
+  --       status.component.lsp(),
+  --       status.component.treesitter(),
+  --       status.component.nav(),
+  --       -- remove the 2nd mode indicator on the right
+  --     }
+  --
+  --     -- return the final configuration table
+  --     return opts
+  --   end,
+  -- },
   {
     "s1n7ax/nvim-window-picker",
     name = "window-picker",
@@ -62,51 +64,52 @@ return {
     config = function() require("window-picker").setup() end,
   },
 
-  {
-    "L3MON4D3/LuaSnip",
-    version = "v2.*",
-    build = "make install_jsregexp",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      "hrsh7th/cmp-cmdline",
-    },
-    config = function(plugin, opts)
-      -- include the default astronvim config that calls the setup call
-      require "plugins.configs.luasnip"(plugin, opts)
-      -- load snippets paths
-      require("luasnip.loaders.from_vscode").lazy_load {
-        -- this can be used if your configuration lives in ~/.config/nvim
-        -- if your configuration lives in ~/.config/astronvim, the full path
-        -- must be specified in the next line
-        paths = {
-          vim.fn.stdpath "config" .. "/lua/user/snippets",
-        },
-      }
-      local cmp = require "cmp"
-      -- `/` cmdline setup.
-      cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
-
-      -- `:` cmdline setup.
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          {
-            name = "cmdline",
-            option = {
-              ignore_cmds = { "Man", "!" },
-            },
-          },
-        }),
-      })
-    end,
-  },
+  -- TODO: Check migration to v4
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   version = "v2.*",
+  --   build = "make install_jsregexp",
+  --   dependencies = {
+  --     "rafamadriz/friendly-snippets",
+  --     "hrsh7th/cmp-cmdline",
+  --   },
+  --   config = function(plugin, opts)
+  --     -- include the default astronvim config that calls the setup call
+  --     require "plugins.configs.luasnip"(plugin, opts)
+  --     -- load snippets paths
+  --     require("luasnip.loaders.from_vscode").lazy_load {
+  --       -- this can be used if your configuration lives in ~/.config/nvim
+  --       -- if your configuration lives in ~/.config/astronvim, the full path
+  --       -- must be specified in the next line
+  --       paths = {
+  --         vim.fn.stdpath "config" .. "/lua/user/snippets",
+  --       },
+  --     }
+  --     local cmp = require "cmp"
+  --     -- `/` cmdline setup.
+  --     cmp.setup.cmdline("/", {
+  --       mapping = cmp.mapping.preset.cmdline(),
+  --       sources = {
+  --         { name = "buffer" },
+  --       },
+  --     })
+  --
+  --     -- `:` cmdline setup.
+  --     cmp.setup.cmdline(":", {
+  --       mapping = cmp.mapping.preset.cmdline(),
+  --       sources = cmp.config.sources({
+  --         { name = "path" },
+  --       }, {
+  --         {
+  --           name = "cmdline",
+  --           option = {
+  --             ignore_cmds = { "Man", "!" },
+  --           },
+  --         },
+  --       }),
+  --     })
+  --   end,
+  -- },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
