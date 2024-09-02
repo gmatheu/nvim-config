51476c8f9b16778dc536e34b15789776746d7622
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-09-02 09:52:36 -0300

    ai: Adding avante.nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -48,7 +48,7 @@ return {
     config = function()
       require("chatgpt").setup {
         openai_params = {
-          model = "gpt-4-turbo",
+          model = "gpt-4o",
           frequency_penalty = 0,
           presence_penalty = 0,
           max_tokens = 300,
@@ -57,7 +57,7 @@ return {
           n = 1,
         },
         openai_edit_params = {
-          model = "gpt-4-turbo",
+          model = "gpt-4o",
           frequency_penalty = 0,
           presence_penalty = 0,
           temperature = 0,
@@ -500,5 +500,51 @@ return {
     enabled = false,
     build = { "pip install -r requirements.txt", ":UpdateRemotePlugins" },
     config = function() require("gemini").setup() end,
+  },
+
+  {
+    "yetone/avante.nvim",
+    -- event = "VeryLazy",
+    -- lazy = false,
+    opts = {
+      -- add any opts here
+    },
+    keys = {
+      { "<leader>aa", function() require("avante.api").ask() end, desc = "avante: ask", mode = { "n", "v" } },
+      { "<leader>ar", function() require("avante.api").refresh() end, desc = "avante: refresh" },
+      { "<leader>ae", function() require("avante.api").edit() end, desc = "avante: edit", mode = "v" },
+    },
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to setup it properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   },
 }
