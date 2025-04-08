5128777a9b24fbbf2b75d18c18a8b78786110a22
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2025-04-08 11:27:23 -0300

    bufferchad: Lists buffers with mapping

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -651,7 +651,7 @@ return {
     "mrquantumcodes/bufferchad.nvim",
     keys = {
       {
-        "<leader>b" .. "b",
+        "<leader>b" .. "bl",
         function() require("bufferchad").BufferChadListBuffers() end,
         desc = "List buffers [bufferchad]",
       },
@@ -663,8 +663,8 @@ return {
     },
     config = function()
       require("bufferchad").setup {
-        mapping = "<leader>bb", -- Map any key, or set to NONE to disable key mapping
-        mark_mapping = "<leader>bm", -- The keybinding to display just the marked buffers
+        -- mapping = "<leader>bb", -- Map any key, or set to NONE to disable key mapping
+        -- mark_mapping = "<leader>bm", -- The keybinding to display just the marked buffers
         order = "LAST_USED_UP", -- LAST_USED_UP (default)/ASCENDING/DESCENDING/REGULAR
         style = "modern", -- default, modern (requires dressing.nvim and nui.nvim), telescope (requires telescope.nvim)
         close_mapping = "<Esc><Esc>", -- only for the default style window.
