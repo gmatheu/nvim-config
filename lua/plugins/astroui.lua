17c6c1ee27933c24fc8f88bc40d31b2f89605fc6
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2025-04-08 11:23:09 -0300

    Changing theme to tokyonight

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -91,6 +91,20 @@ return {
         LSPLoading10 = "⠏",
       },
     },
+    dependencies = {
+      {
+        "folke/tokyonight.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+      },
+      {
+        "projekt0n/github-nvim-theme",
+        enabled = false,
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function() require("github-theme").setup() end,
+      },
+    },
   },
   {
     "onsails/lspkind.nvim",
