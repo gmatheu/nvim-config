cf0aedbb7e9227b1a9780775328d4b0e19a05ef8
Gonzalo Matheu (framework) <gonzalo.matheu@intive.com>
2025-10-27 09:05:06 -0300

    ai: Adding vim-daddob-ui

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -887,4 +887,26 @@ return {
       }
     end,
   },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    keys = {
+      { "<LocalLeader>dd", "<cmd>DBUIToggle<CR>", desc = "Toggle DB UI [dadbod]" },
+      { "<LocalLeader>db", "<cmd>DBUIFindBuffer<CR>", desc = "Find DB Buffer [dadbod]" },
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
